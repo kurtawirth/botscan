3d628edffb8225e5f106c34f747b54bcb1f17ccc
Kurt Wirth <kurtawirth@gmail.com>
2018-04-29 14:19:27 -0400

    Threshhold Customization
    Added an argument to allow for a user to customize the botrnot threshhold for consideration as a bot. Default set to .899.

--- R/botscan.R
+++ R/botscan.R
@@ -8,24 +8,30 @@
 #' @author Kurt Wirth
 #'
 #' @param x A Twitter search query in quotation marks.
+#' 
+#' @param y Number, less than one, that determines which botornot probability 
+#' threshhold to return. Default is set at 0.899. Only users estimated to be 
+#' more likely than the threshhold provided will be regarded as a bot.
 #'
 #' @return Percentage of users within the requested conversation that are
-#' estimated by botornot to have at least a 90% chance of being a bot.
+#' estimated by botornot to be a bot.
 #'
-#' Examples fail unless you have created and installed Twitter tokens, per
-#' instructions provided at http://rtweet.info/articles/auth.html.
 #' 
 #' @examples
 #' \dontrun{botscan("#rtweets")}
 #' \dontrun{botscan("trump")}
+#' 
+#' Examples fail unless you have created and installed Twitter tokens, per
+#' instructions provided at http://rtweet.info/articles/auth.html.
+#' 
 #' @export
 
-botscan <- function(x){
+botscan <- function(x,y = 0.899){
   tweets <- rtweet::search_tweets(x, n = 1000, include_rts = FALSE)
 
   userbots <- botrnot::botornot(tweets, fast = TRUE)
 
-  nbots <- sum(userbots$prob_bot > "0.899")
+  nbots <- sum(userbots$prob_bot > y)
 
   n <- length(unique(tweets$screen_name))
 
