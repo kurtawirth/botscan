78d2fbc0cc008c42bf79703097c9ab00f829f244
Ryan T Moore <ryantmoore@gmail.com>
2018-08-09 14:00:45 -0400

    Format botscan documentation

--- R/botscan.R
+++ R/botscan.R
@@ -2,27 +2,27 @@
 #'
 #' Scans Twitter Conversations For Bots
 #'
-#' Takes a Twitter query and produces the percentage of users within that
+#' Takes a Twitter query and produces the proportion of users within that
 #' conversation that are likely to be bots.
 #'
 #' @author Kurt Wirth
 #'
 #' @param x A Twitter search query in quotation marks.
 #' 
-#' @param th Number, less than one, that determines which botornot probability 
+#' @param th A number between zero and one that determines which botornot probability 
 #' threshhold to return. Default is set at 0.899. Only users estimated to be 
 #' more likely than the threshhold provided will be regarded as a bot.
 #' 
-#' @param user_level A logical argument that determines whether to analyze
-#' conversation-level or user-level data. Default is set to FALSE, understood
+#' @param user_level A logical that determines whether to analyze
+#' conversation-level or user-level data. Default is set to \code{FALSE}, understood
 #' as analyzing conversation-level data.
 #'
 #' @return Percentage of users, equal to or less than 1 (read: 100%) and zero 
 #' within the requested conversation that are estimated by botornot to be a bot.
 #' 
-#' When "user_level" is set to TRUE, the value returned is the percentage of
+#' When \code{user_level} is set to \code{TRUE}, the value returned is the percentage of
 #' users within the conversation that are estimated to be bots. When
-#' "user_level" is set to FALSE, the value returned is the percentage of the
+#' \code{user_level} is set to \code{FALSE}, the value returned is the percentage of the
 #' conversation that is estimated to be authored by bots.
 #'
 #' @examples
