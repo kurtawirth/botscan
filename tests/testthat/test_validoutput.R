55669559aa3268384ce3c96eb8a1b8e0db671554
ryantmoore <ryantmoore@gmail.com>
2019-06-19 13:21:39 -0400

    Initialize test data

--- tests/testthat/test_validoutput.R
+++ tests/testthat/test_validoutput.R
@@ -1,13 +1,13 @@
 context("ValidProportions")
 library(botscan)
 
-example <- botscan("#rstats")
+load("test_object.RData")
 
 test_that("proportions calculated are between 0 and 1, inclusive", 
           {
-            expect_lte(example$prop_user_level_bots, 1)
-            expect_gte(example$prop_user_level_bots, 0)
-            expect_lte(example$prop_convo_level_bots, 1)
-            expect_gte(example$prop_convo_level_bots, 0)
+            expect_lte(test_object$prop_user_level_bots, 1)
+            expect_gte(test_object$prop_user_level_bots, 0)
+            expect_lte(test_object$prop_convo_level_bots, 1)
+            expect_gte(test_object$prop_convo_level_bots, 0)
             }
           )
